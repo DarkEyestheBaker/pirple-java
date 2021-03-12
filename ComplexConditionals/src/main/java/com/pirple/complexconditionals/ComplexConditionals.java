5e778c0a32645263657ded3b5a9bb75affd493e8
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-03-12 17:27:56 -0500

    Complex Conditionals

---
+++ ComplexConditionals/src/main/java/com/pirple/complexconditionals/ComplexConditionals.java
@@ -0,0 +1,28 @@
+
+
+package com.pirple.complexconditionals;
+
+import java.util.*;
+
+public class ComplexConditionals {
+
+    
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String input = reader.next();
+        String sOne="abc";
+        String sTwo="z";
+        
+        if(input.contains(sOne))
+        {
+            System.out.println("TRUE!");
+        }
+        else
+        {
+            System.out.println("FALSE!");
+        }
+        }
+        
+    }
+    
+
